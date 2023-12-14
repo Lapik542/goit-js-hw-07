6789a0d4e46c24ea282eeb8a8b4b078af3d9781e
Гавриш Олександр <132341505+Lapik542@users.noreply.github.com>
2023-12-14 14:26:58 +0200

    Update task-01.js

--- js/task-01.js
+++ js/task-01.js
@@ -1,18 +1,11 @@
 const list = document.querySelector('#categories');
-const childeList = list.children;
-console.log(`Number of categories: ${childeList.length}`);
+const childList = list.children;
+console.log(`Number of categories: ${childList.length}`);
 
-const animals = list.querySelector('h2');
-console.log(`Category: ${animals.textContent}`);
-const elementAnimal = childeList[0].querySelectorAll('li');
-console.log(`Elements: ${elementAnimal.length}`);
+[...childList].forEach(category => {
+    const categoryHeader = category.querySelector('h2');
+    const categoryElements = category.querySelectorAll('li');
 
-const product = list.querySelectorAll('#categories>li>h2')
-console.log(`Category: ${product[1].textContent}`);
-const elementProduct = childeList[1].querySelectorAll('li')
-console.log(`Elements: ${elementProduct.length}`);
-
-const technologies = list.querySelectorAll('#categories>li>h2')
-console.log(`Category: ${technologies[2].textContent}`);
-const elementTechnologies = childeList[2].querySelectorAll('li')
-console.log(`Elements: ${elementTechnologies.length}`);
+    console.log(`Category: ${categoryHeader.textContent}`);
+    console.log(`Elements: ${categoryElements.length}`);
+});
