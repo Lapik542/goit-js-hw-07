a3d748f1b82969e4ea2dc913f4c3103865a41c06
Гавриш Олександр <132341505+Lapik542@users.noreply.github.com>
2023-12-23 23:10:46 +0200

    Update task-06.js

--- js/task-06.js
+++ js/task-06.js
@@ -5,37 +5,36 @@ const destroyBtn = document.querySelector('[data-destroy]');
 const boxesContainer = document.getElementById('boxes');
 
 const createBoxes = (amount) => {
-   const amount = input.valueAsNumber;
-
   if (amount < 1 || amount > 100 || isNaN(amount)) {
     alert('Please enter a number between 1 and 100.');
     return;
   }
 
   let currentSize = 30;
-   const elements = [];
+  const elements = [];
   for (let i = 0; i < amount; i++) {
     const box = document.createElement('div');
     box.classList.add('box');
     box.style.width = `${currentSize}px`;
     box.style.height = `${currentSize}px`;
-     box.style.backgroundColor = getRandomHexColor();
-     elements.push(box);
+    box.style.backgroundColor = getRandomHexColor();
+    elements.push(box);
     currentSize += 10;
   }
-boxesContainer.append(...elements);
+  boxesContainer.append(...elements);
   input.value = '';
 };
 
+const destroyBoxes = () => {
+  boxesContainer.innerHTML = '';
+};
 
-    const destroyBoxes = () => {
-      boxesContainer.innerHTML = '';
-    };
 createBtn.addEventListener('click', () => {
-   const amount = input.valueAsNumber;
-   destroyBoxes();
-   createBoxes(amount);
-    });
+  const amountValue = input.valueAsNumber;
+  destroyBoxes();
+  createBoxes(amountValue);
+});
+
 destroyBtn.addEventListener('click', () => {
-   destroyBoxes();
-    });
+  destroyBoxes();
+});
